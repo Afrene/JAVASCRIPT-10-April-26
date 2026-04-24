8b14e9febada54b2b5fc65fe44d2e63f2cadb9af
Afrene <afrene.zebeen@gmail.com>
2026-04-24 21:48:24 +0600

    for of loop

--- Module-15-Loop/for-of.js
+++ Module-15-Loop/for-of.js
@@ -1,7 +1,92 @@
-const numbers = [12,98,45,63,21,72];
+//************************************* */
+           // Code 1
+//************************************ */     
 
-for ( const num of numbers){
-    console.log(num);
+// const numbers = [12,98,45,63,21,72];
+
+// for ( const num of numbers){
+//     console.log(num);
+// }
+
+// output
+// -----------
+// 12
+// 98
+// 45
+// 63
+// 21
+// 72
+
+//************************************ */
+            // Code 2
+//*********************************** */   
+
+const fruits = ['orange', 'apple', 'banana', 'jackfruit','watermelon'];
+
+for(const fruit of fruits){
+    console.log(fruit);
 }
+
+// output
+// ----------
+// apple
+// banana
+// jackfruit
+// watermelon
+
+//*********************************** */
+          //Practice - for of loop
+//**************************************** */   
+
+// Practice 1
+// Subjects names print
+//---------------------------------
+
+// const subjects = ['Math', 'Physics','Chemistry','Bangla','English'];
+
+// for(const subject of subjects){
+//     console.log(subject);
+// }
+
+// output
+// -----------
+// Physics
+// Chemistry
+// Bangla
+// English
+
+//******************************************* */
+// Practice 2
+// foods names and print each food name
+
+const foods = ['Burger', 'Pizza', 'Coke', 'Beef'];
+
+for(const food of foods){
+    console.log(food);
+}
+
+// output 
+// --------
+// Burger
+// Pizza
+// Coke
+// Beef
+
+//****************************************** */
+//Practice 3
+// DOB of Family members and print each family member's DOB
+
+const membersDOB = ['12 June', '23 August', '15 january','23 April'];
+
+for(const memberDOB of membersDOB){
+    console.log(memberDOB);
+}
+
+// output
+// ------------
+// 12 June
+// 23 August
+// 15 january
+// 23 April
 
 
--- Module-15-Loop/note-15.md
+++ Module-15-Loop/note-15.md
@@ -9,7 +9,7 @@ ver = should not use
 
 ### :book: for of loop
 
-## code -1 
+## :brain: code -1 
 ``` js
 
 const numbers = [12,98,45,63,21,72];
@@ -29,5 +29,99 @@ for ( const num of numbers){
 63
 21
 72
+
+```
+
+## :brain: code 2
+
+``` js
+const fruits = ['orange', 'apple', 'banana', 'jackfruit','watermelon'];
+
+for(const fruit of fruits){
+    console.log(fruit);
+}
+
+```
+## output
+```
+output
+----------
+apple
+banana
+jackfruit
+watermelon
+
+```
+
+### :rocket: Practice for of loop
+
+## :book: Practice 1
+
+# :bulb: print subjects name
+
+``` js
+
+const subjects = ['Math', 'Physics','Chemistry','Bangla','English'];
+
+for(const subject of subjects){
+    console.log(subject);
+}
+
+```
+## output
+
+```
+const subjects = ['Math', 'Physics','Chemistry','Bangla','English'];
+
+for(const subject of subjects){
+    console.log(subject);
+}
+
+```
+
+## :book: Practice 2
+
+# :bulb: print foods name
+
+``` js
+
+const foods = ['Burger', 'Pizza', 'Coke', 'Beef'];
+
+for(const food of foods){
+    console.log(food);
+}
+
+```
+
+## output
+
+```
+Burger
+Pizza
+Coke
+Beef
+
+```
+
+## :book: Practice 3
+# :bulb: DOB of Family members and print each family member's DOB
+
+``` js
+
+const membersDOB = ['12 June', '23 August', '15 january','23 April'];
+
+for(const memberDOB of membersDOB){
+    console.log(memberDOB);
+}
+
+```
+
+## output
+
+```
+12 June
+23 August
+15 january
+23 April
 
 ```
